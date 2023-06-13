3f62dd6abc63d197d50d19dff5b9c53691545ad4
Dylan-1024040 <1024040@hr.nl>
2023-06-13 12:21:53 +0200

    500 internal server fout

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template, jsonify, url_for, redirect, request, s
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/info')
 def hello():
-    return "Hello"
-# Press the green button in the gutter to run the script.
+    return {"name": "Dylan",
+            "age": 22,
+            "hobbies": ["guitar", "going to the gym", "music", "football"],
+            "gender": "male",
+            "location": "Rotterdam",
+            "fav cities": ["Lisboa", "Paris", "Barcelona", "New York City", "Brussel/Bruxelles", "Valencia/Valéncia",
+                           "Bordeaux"]
+
+            }
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
