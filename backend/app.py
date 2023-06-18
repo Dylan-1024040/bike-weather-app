c5f62511c0ae503223776e4d96776938d0edec3b
Dylan-1024040 <1024040@hr.nl>
2023-06-18 14:11:04 +0200

    Een begin gemaakt van de zoekbalk

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, jsonify, url_for, redirect, request, session
 from flask_cors import CORS
+import json
+import urllib.request
 
 app = Flask(__name__)
 CORS(app)
 
-#@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def weer():
+    if request.method == 'POST':
+        city = request.form['city']
+    else:
+        city = 'Atlantis'
+
+    source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=11.00&appid=7448d089b3ccc0fd86b7a71672c3cf9c')
+
 @app.route('/api/info')
 def api():
-    data = {"location": "Rotterdam"}
+    data = {"location": "Rotterdam",
+            "temperature": "20"}
     return jsonify(data)
 
 
