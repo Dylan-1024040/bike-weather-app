bd06ccb9bca325e04e0f13e783a1fbf0adfb2a86
Dylan-1024040 <1024040@hr.nl>
2024-05-20 17:11:41 +0200

    React en flask staan nu gereed

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+import os
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello Dylan!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
