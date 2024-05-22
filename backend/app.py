3ccfb17d493d2f1a548a839af9c5c4847bd5bc56
Dylan-1024040 <1024040@hr.nl>
2024-05-22 12:09:10 +0200

    in de app.py heb ik routes aangemaakt waarin het weer tevoorschijn komt en de instellingen

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Dylan!'
+    print('het is gelukt!')
+    return jsonify ({'Name': 'Dylan', 'Age': 23, 'location': 'New York'})
+
+@app.route('/api/weather')
+def weather_get():
+    pass
+
+@app.route('/api/settings', methods=['GET', 'POST'])
+def settings():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
