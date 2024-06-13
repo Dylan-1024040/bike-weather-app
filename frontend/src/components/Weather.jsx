63d0ffc9b8e3647b4b144a9f0f667a48971ae466
Dylan-1024040 <1024040@hr.nl>
2024-06-13 13:51:29 +0200

    weather.jsx aangemaakt

--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -1,11 +1,51 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Weather = () => {
+const Weather = ({ userId }) => {
+    const [dataWeather, setDataWeather ] = useState({
+        location: '',
+        departure: '',
+        okay_to_bike: [],
+    });
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            if (userId) {
+                try {
+                    const response = await axios.get(`/api/weather/${userId}`);
+                    setDataWeather(response.data);
+                } catch (error) {
+                    console.error('Fout bij ophalen weer data: ', error);
+                }
+            }
+        };
+
+        fetchWeather();
+    }, [userId]);
+
+
+
     return (
         <div>
-            <h2>weer voospellingS</h2>
+            <h2>Weer voospelling</h2>
+            <p>Locatie: {dataWeather.location}</p>
+            <p>VertrekTijd: {dataWeather.departure}</p>
+            {dataWeather.okay_to_bike.map((data, index) => (
+                <div key={index}>
+                    <p>Datum: {data.date}</p>
+                    <p>
+                        Fietsweer: 
+                        {data.bike_okay ? (
+                            <img src="/happyBike.png" alt="sun on bike" style={{ width: '30px', height: '30px'}} />
+                        ) : (
+                            <img src="/bikeinside.png" alt="bike in house" style={{ width: '30px', height: '30px'}} />
+                        )}
+                    </p>
+                </div>
+            ))}
         </div>
 
     );
 };
+
+export default Weather;
