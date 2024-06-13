148214800a1f1ac1c66cd9d56a121ab31290098d
Dylan-1024040 <1024040@hr.nl>
2024-06-13 13:25:00 +0200

    app.jsx page aangemaakt, en een start gemaakt met de weather.jsx and settings.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-dom';
-// import axios from 'axios';
-// import Settings from './components/Settings';
-// import Weather from './components/weather';
+import axios from 'axios';
+import Settings from './components/Settings';
+import Weather from './components/Weather';
 import './App.css';
 
 const App = () => {
+  const [userId, setUserId] = useState(null);
+  const [initSettings, setInitSettings] = useState({
+    location: '',
+    knockOutFactors: {
+      wind: 0.0,
+      rain: 25,
+      cold: 0,
+      hot: 0,
+      snow: 0,
+    },
+    timepreferred: '08:00',
+  });
+
+  useEffect(() => {
+    const fetchSettings = async () => {
+      try {
+        const response = await axios.get('/api/settings');
+        const { user_id, ...settings } = response.data;
+        setUserId(user_id);
+        setInitSettings(settings);
+      } catch (error) {
+        console.error('Fout bij ophalen instellingen: ', error);
+      }
+
+    };
+    fetchSettings();
+  }, []);
+
+
   return (
-    // <Router>
+    <Router>
       <div className="App">
         <h1>Weather App</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/settings">Instellingen</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Weather userId={userId} />} />
+          <Route path="/settings" element={<Settings initSettings={initSettings} setUserId={setUserId} />} /> 
+        </Routes>
       </div>
-    // </Router>  
+   </Router>  
   )
 }
 
---
+++ frontend/src/components/Weather.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Weather = () => {
+    return (
+        <div>
+            <h2>weer voospellingS</h2>
+        </div>
+
+    );
+};
