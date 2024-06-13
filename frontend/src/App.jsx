b28e15610ed3dd21184907a7b9454664488a48a6
Dylan-1024040 <1024040@hr.nl>
2024-06-13 12:14:55 +0200

    begin van een nieuwe app.jsx bestand

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-dom';
+// import axios from 'axios';
+// import Settings from './components/Settings';
+// import Weather from './components/weather';
+import './App.css';
+
+const App = () => {
+  return (
+    // <Router>
+      <div className="App">
+        <h1>Weather App</h1>
+      </div>
+    // </Router>  
+  )
+}
+
+export default App;
